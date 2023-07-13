ea36942bce8a8e739dd2fbeb313533c609acab15
zhoujinjina <3513776811@qq.com>
2023-07-13 20:24:43 +0800

    update

--- src/components/circleRating/CircleRating.jsx
+++ src/components/circleRating/CircleRating.jsx
@@ -10,7 +10,7 @@ const CircleRating = ({ rating }) => {
             <CircularProgressbar
                 value={rating}
                 maxValue={10}
-      
+                text={rating}
                 styles={buildStyles({
                     pathColor:
                         rating < 5 ? "red" : rating < 7 ? "orange" : "green",
