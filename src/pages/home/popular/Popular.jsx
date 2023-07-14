2747ebc8ad494d756174d07a2b4cf8f422f10cba
zhoujinjina <3513776811@qq.com>
2023-07-14 10:34:58 +0800

    update

--- src/pages/home/popular/Popular.jsx
+++ src/pages/home/popular/Popular.jsx
@@ -7,22 +7,21 @@ import SwitchTabs from "../../../components/switchTabs/SwitchTabs";
 import useFetch from "../../../hooks/useFetch";
 
 const Popular = () => {
-    const [endpoint, setEndpoint] = useState("day");
-    const { data, loading } = useFetch(`/popular/movie/${endpoint}`);
-    console.log()
-    const onTabChange = (tab) => {
-        setEndpoint(tab === "Day" ? "day" : "week");
-    };
-    return (
-        <div><div className="carouselSection">
-            <ContentWrapper>
-                <span className="carouselTitle">Trending</span>
-                <SwitchTabs data={["Day", "Week"]} onTabChange={onTabChange} />
-            </ContentWrapper>
-            <Carousel data={data?.results} loading={loading} />
-        </div>
-        </div>
-    );
+  const [endpoint, setEndpoint] = useState("day");
+  const { data, loading } = useFetch(`/popular/movie/${endpoint}`);
+  console.log();
+  const onTabChange = (tab) => {
+    setEndpoint(tab === "Day" ? "day" : "week");
+  };
+  return (
+    <div className="carouselSection">
+      <ContentWrapper>
+        <span className="carouselTitle">Trending</span>
+        <SwitchTabs data={["Day", "Week"]} onTabChange={onTabChange} />
+      </ContentWrapper>
+      <Carousel data={data?.results} loading={loading} />
+    </div>
+  );
 };
 
 export default Popular;
