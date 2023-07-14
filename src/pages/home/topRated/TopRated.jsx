3eb4491b07650ea2c2664723cd0db05e77186737
zhoujinjina <3513776811@qq.com>
2023-07-14 10:41:57 +0800

    update

--- src/pages/home/topRated/TopRated.jsx
+++ src/pages/home/topRated/TopRated.jsx
@@ -7,12 +7,20 @@ import SwitchTabs from "../../../components/switchTabs/SwitchTabs";
 import useFetch from "../../../hooks/useFetch";
 
 const TopRated = () => {
- 
+    const [endpoint, setEndpoint] = useState("day");
+    const { data, loading } = useFetch(`/trending/movie/${endpoint}`);
+    console.log();
+    const onTabChange = (tab) => {
+      setEndpoint(tab === "Day" ? "day" : "week");
+    };
     return (
-       <div>
-        
-       </div>
+      <div className="carouselSection">
+        <ContentWrapper>
+          <span className="carouselTitle">Popular</span>
+          <SwitchTabs data={["Day", "Week"]} onTabChange={onTabChange} />
+        </ContentWrapper>
+        <Carousel data={data?.results} loading={loading} />
+      </div>
     );
-};
-
+  };
 export default TopRated;
