1e8a0c88740a33467ccd0bd263e5241540fe9bde
zhoujinjina <3513776811@qq.com>
2023-07-18 18:22:42 +0800

    update

--- src/components/movieCard/MovieCard.jsx
+++ src/components/movieCard/MovieCard.jsx
@@ -9,11 +9,34 @@ import CircleRating from "../circleRating/CircleRating";
 import Genres from "../genres/Genres";
 import PosterFallback from "../../assets/no-poster.png";
 
-const MovieCard = ({ }) => {
-
-    return (
-       <div></div>
-    );
+const MovieCard = ({ data, fromSearch, mediaType }) => {
+  const { url } = useSelector((state) => state.home);
+  const navigate = useNavigate();
+  const posterUrl = data.poster_path
+    ? url.poster + data.poster_path
+    : PosterFallback;
+  return (
+    <div
+      className="movieCard"
+      onClick={() => navigate(`/${data.media_type || mediaType}/${data.id}`)}
+    >
+      <div className="posterBlock">
+        <Img className="posterImg" src={posterUrl} />
+        {!fromSearch && (
+          <React.Fragment>
+            <CircleRating rating={data.vote_average.toFixed(1)} />
+            <Genres data={data.genre_ids.slice(0, 2)} />
+          </React.Fragment>
+        )}
+      </div>
+      <div className="textBlock">
+        <span className="title">{data.title || data.name}</span>
+        <span className="date">
+          {dayjs(data.release_date).format("MMM D, YYYY")}
+        </span>
+      </div>
+    </div>
+  );
 };
 
 export default MovieCard;
