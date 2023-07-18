e3c0f4537f162080354df027d099d76f3573c345
zhoujinjina <3513776811@qq.com>
2023-07-18 21:53:29 +0800

    完成电影详情页

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -17,7 +17,6 @@ const Details = () => {
     const { data: credits, loading: creditsLoading } = useFetch(
         `/${mediaType}/${id}/credits`
     );
-console.log(data);
     return (
         <div>
             <DetailsBanner video={data?.results?.[0]} crew={credits?.crew} />
