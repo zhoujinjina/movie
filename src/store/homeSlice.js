d72e45f255a630c701136e92d1b3aab209db6c14
zhoujinjina <3513776811@qq.com>
2023-07-19 21:05:35 +0800

    存储电影类型以及请求对应的api

--- src/store/homeSlice.js
+++ src/store/homeSlice.js
@@ -16,7 +16,7 @@ export const homeSlice = createSlice({
     },
 });
 
-// Action creators are generated for each case reducer function
+
 export const { getApiConfiguration, getGenres } = homeSlice.actions;
 
 export default homeSlice.reducer;
