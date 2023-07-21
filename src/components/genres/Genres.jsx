4af5a6e5e1ea2b638084776fc9f46a7f117c2368
zhoujinjina <3513776811@qq.com>
2023-07-21 13:46:16 +0800

    封装类型组件

--- src/components/genres/Genres.jsx
+++ src/components/genres/Genres.jsx
@@ -4,20 +4,20 @@ import { useSelector } from "react-redux";
 import "./style.scss";
 // 种类
 const Genres = ({ data }) => {
-    const { genres } = useSelector((state) => state.home);
-
-    return (
-        <div className="genres">
-            {data?.map((g) => {
-                if (!genres[g]?.name) return;
-                return (
-                    <div key={g} className="genre">
-                        {genres[g]?.name}
-                    </div>
-                );
-            })}
-        </div>
-    );
+  const { genres } = useSelector((state) => state.home);
+  console.log(genres);
+  return (
+    <div className="genres">
+      {data?.map((g) => {
+        if (!genres[g]?.name) return;
+        return (
+          <div key={g} className="genre">
+            {genres[g]?.name}
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Genres;
