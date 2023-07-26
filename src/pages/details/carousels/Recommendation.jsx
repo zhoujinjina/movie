fd699bf90949816d8d9d1e4505670ab156d1660f
zhoujinjina <3513776811@qq.com>
2023-07-26 20:56:19 +0800

    完成猜你喜欢模块

--- src/pages/details/carousels/Recommendation.jsx
+++ src/pages/details/carousels/Recommendation.jsx
@@ -4,7 +4,7 @@ import Carousel from "../../../components/carousel/Carousel";
 import useFetch from "../../../hooks/useFetch";
 
 const Recommendation = ({ mediaType, id }) => {
-  //获取相似的电影
+  //获取猜你喜欢
   const { data, loading, error } = useFetch(
     `/${mediaType}/${id}/recommendations?language=zh-CN`
   );
